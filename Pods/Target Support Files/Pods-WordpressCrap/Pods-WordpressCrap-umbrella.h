e593bfaca44a441b6c6c752d2f9bcd5e35a2dfc5
David Seek <davidseek1986@gmail.com>
2016-12-06 16:29:11 +0100

    init

---
+++ Pods/Target Support Files/Pods-WordpressCrap/Pods-WordpressCrap-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_WordpressCrapVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_WordpressCrapVersionString[];
+
---
+++ WordpressCrap/ViewController.swift
@@ -0,0 +1,49 @@
+//
+//  ViewController.swift
+//  WordpressCrap
+//
+//  Created by David Seek on 12/6/16.
+//  Copyright © 2016 David Seek. All rights reserved.
+//
+
+import UIKit
+import WordPressEditor
+
+class ViewController: WPEditorViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
+extension ViewController: WPEditorViewControllerDelegate {
+    
+    func editorDidBeginEditing(_ editorController: WPEditorViewController!) {
+        //
+    }
+    
+    func editorDidEndEditing(_ editorController: WPEditorViewController!) {
+        //
+    }
+    
+    func editorDidFinishLoadingDOM(_ editorController: WPEditorViewController!) {
+        //
+    }
+    
+    func editorShouldDisplaySourceView(_ editorController: WPEditorViewController!) -> Bool {
+        //
+        return true
+    }
+    
+    func editorTitleDidChange(_ editorController: WPEditorViewController!) {
+        //
+    }
+    
+    func editorTextDidChange(_ editorController: WPEditorViewController!) {
+        //
+    }
+    
+    func editorDidPressMedia(_ editorController: WPEditorViewController!) {
+        //
+    }
+}
